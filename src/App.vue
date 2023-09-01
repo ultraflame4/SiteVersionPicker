47f22f00d71eec5f7b6899c5892c62041056a37d
ultr42 <ultr42@skiff.com>
2023-09-01 16:41:07 +0800

    Changed version picker stylng

--- src/App.vue
+++ src/App.vue
@@ -1,109 +1,78 @@
 <template>
 
-    <h1>Site Version Picker</h1>
-    <hr/>
-    <template v-if="autoRedirectActive">
-        <h2>Auto Redirect to <span style="color:aquamarine">{{latestRelease}}</span> in <span style="background: var(--bg-2);padding: 0 0.25em;border-radius: 0.4em">{{ counter }} second</span>!</h2>
-        <h1 style="margin-bottom: 0"  @click="autoRedirectActive=false">[<span style="color:cadetblue;cursor: pointer">Click Me!</span>]</h1>
-        <h2 style="margin: 0">or</h2>
-        <h1 style="margin: 0">[<span style="color:indianred">Shift + C</span>]</h1>
-        <h2 style="margin-top: 0"> to Cancel!</h2>
-
-    </template>
-    <template v-else>
-        <h1>[ <span style="color:indianred">STOPPED</span> ]</h1>
-    </template>
-    <hr/>
-
-    <h2>View Specific Version</h2>
-
-    <ul id="version-list">
-        <li v-for="item in SiteVersions">
-            <a :href="`https://ultraflame4.github.io/${item.name}`">
-                &xrArr;{{ item.name }}&xlArr;
-            </a>
-        </li>
-    </ul>
+  <h1>Site Version Picker</h1>
+  <template v-if="autoRedirectActive">
+    <p>
+      Auto-Redirect to latest <span class="version_text">{{ latestRelease.trim() }}</span>
+      in {{ counter }}s
+    </p>
+    <button @click="autoRedirectActive=false">
+      Select Version
+    </button>
+  </template>
+  <template v-else>
+      Auto-Redirect canceled
+    <p>
+      Select a version to continue
+    </p>
+    <select v-model="selected_version">
+      <option v-for="version in SiteVersions.reverse()" :value="version.name">
+        {{ version.name }}
+      </option>
+    </select>
+  </template>
 
 
 </template>
 <script setup lang="ts">
 import {latestRelease, SiteVersions} from "./tools.ts";
-import {ref} from "vue";
+import {ref, watch} from "vue";
 
 const autoRedirectActive = ref(true)
 const counter = ref(2)
+const selected_version = ref("none")
 
+const id = setInterval(() => {
+  counter.value--;
+  if (counter.value < 1) {
+    clearInterval(id)
+    setTimeout(() => {
+      if (autoRedirectActive.value) {
+        selected_version.value=latestRelease.value
+      }
+    }, 1200)
+  }
+}, 1000)
+watch(selected_version, (value)=>{
+  if (SiteVersions.value.find(x=>x.name===value))
+    window.location.assign(`https://ultraflame4.github.io/${value}`)
+})
 
-const id = setInterval(()=>{
-    counter.value--;
-    if (counter.value < 1){
-        clearInterval(id)
-        setTimeout(()=>{
-            if (autoRedirectActive.value) {
-                window.location.assign(`https://ultraflame4.github.io/${latestRelease.value}`)
-            }
-        },1200)
-    }
-},1000)
-document.addEventListener('keypress',ev => {
-    if (ev.shiftKey && ev.key.toLowerCase() == "c") {
-        autoRedirectActive.value=false
-        clearInterval(id)
-    }
+document.addEventListener('keypress', ev => {
+  if (ev.shiftKey && ev.key.toLowerCase() == "c") {
+    autoRedirectActive.value = false
+    clearInterval(id)
+  }
 })
 </script>
 
 <style scoped lang="scss">
 
-#version-list {
-    list-style-type: none;
-    padding-left: 0;
-    filter: drop-shadow(0 0 3px black);
+.version_text {
 
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    gap: 0.3em;
-    & > li {
+  text-transform: lowercase;
+  font-family: "Poppins";
+  letter-spacing: 1px;
+  font-size: 0.9em;
+  font-weight: 600;
+  background: var(--bg-2);
+  padding: 0 0.45rem;
+  border-radius: 0.35rem;
 
-        &:hover {
+}
 
-            & > a {
-                color: aquamarine;
-            }
-
-            &>a::before{
-                content: "[";
-            }
-            &>a::after{
-                content: "]";
-            }
-        }
-
-        & > a {
-            display: flex;
-            height: 2.25rem;
-            align-items: center;
-
-            font-weight: 800;
-            font-size: 1.3em;
-            text-decoration: none;
-            letter-spacing: 0.2em;
-            color: beige;
-
-            transition: color 300ms ease-in-out;
-            text-transform: uppercase;
-            overflow: clip;
-            width: 100%;
-            position: relative;
-
-        }
-
-
-    }
-
+p {
+  letter-spacing: 0.1px;
 }
 
 
