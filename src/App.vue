66ed935edcbf298bdf3b7a880ed82ebcb0aedf25
ultr42 <ultr42@skiff.com>
2023-09-13 23:32:19 +0800

    Fixed recursion issue + legacy polyfills

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
 
-  <h1>Site Version Picker</h1>
-  <template v-if="autoRedirectActive">
-    <p>
-      Auto-Redirect to latest <span class="version_text">{{ latestRelease.trim() }}</span>
-      in {{ counter }}s
-    </p>
-    <button @click="autoRedirectActive=false">
-      Select Version
-    </button>
-  </template>
-  <template v-else>
-      Auto-Redirect canceled
-    <p>
-      Select a version to continue
-    </p>
-    <select v-model="selected_version">
-      <option v-for="version in SiteVersions.reverse()" :value="version.name">
-        {{ version.name }}
-      </option>
-    </select>
-  </template>
+    <h1>Site Version Picker</h1>
+    <template v-if="autoRedirectActive">
+        <p>
+            Auto-Redirect to latest <span class="version_text">{{ latestRelease.trim() }}</span>
+            in {{ counter }}s
+        </p>
+        <button @click="autoRedirectActive=false">
+            Select Version
+        </button>
+    </template>
+    <template v-else>
+        Auto-Redirect canceled
+        <p>
+            Select a version to continue
+        </p>
+        <select v-model="selected_version">
+            <option v-for="version in SiteVersions.toReversed()" :value="version.name">
+                {{ version.name }}
+            </option>
+        </select>
+    </template>
 
 
 </template>
@@ -33,27 +33,33 @@ const counter = ref(3)
 const selected_version = ref("none")
 
 const id = setInterval(() => {
-  counter.value--;
-  if (counter.value < 1) {
-    clearInterval(id)
-    setTimeout(() => {
-      if (autoRedirectActive.value) {
-        console.log("Should be redirecting to",latestRelease.value)
-        selected_version.value=latestRelease.value
-      }
-    }, 1200)
-  }
+    counter.value--;
+    if (counter.value < 1) {
+        clearInterval(id)
+        auto_redirect()
+    }
 }, 1000)
-watch(selected_version, (value)=>{
-  if (SiteVersions.value.find(x=>x.name.trim()===value.trim()))
-    window.location.assign(`https://ultraflame4.github.io/${value.trim()}`)
+
+function auto_redirect() {
+    setTimeout(() => {
+        if (autoRedirectActive.value) {
+            console.log("Should be redirecting to", latestRelease.value)
+            selected_version.value = latestRelease.value
+        }
+    }, 1200)
+}
+
+watch(selected_version, (value) => {
+    if (SiteVersions.value.find(x => x.name.trim() === value.trim())) {
+        window.location.assign(`https://ultraflame4.github.io/${value.trim()}`)
+    }
 })
 
 document.addEventListener('keypress', ev => {
-  if (ev.shiftKey && ev.key.toLowerCase() == "c") {
-    autoRedirectActive.value = false
-    clearInterval(id)
-  }
+    if (ev.shiftKey && ev.key.toLowerCase() == "c") {
+        autoRedirectActive.value = false
+        clearInterval(id)
+    }
 })
 </script>
 
@@ -61,19 +67,19 @@ document.addEventListener('keypress', ev => {
 
 .version_text {
 
-  text-transform: lowercase;
-  font-family: "Poppins";
-  letter-spacing: 1px;
-  font-size: 0.9em;
-  font-weight: 600;
-  background: var(--bg-2);
-  padding: 0 0.45rem;
-  border-radius: 0.35rem;
+    text-transform: lowercase;
+    font-family: "Poppins";
+    letter-spacing: 1px;
+    font-size: 0.9em;
+    font-weight: 600;
+    background: var(--bg-2);
+    padding: 0 0.45rem;
+    border-radius: 0.35rem;
 
 }
 
 p {
-  letter-spacing: 0.1px;
+    letter-spacing: 0.1px;
 }
 
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import legacy from '@vitejs/plugin-legacy'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  base:"/SiteVersionPicker"
+  plugins: [
+      vue(),
+      legacy()
+  ],
+  base:"/SiteVersionPicker",
 })
